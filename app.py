0c3d3205a76b4957d1a0435b0324833c32116b6f
kpandey1012 <33618407+kpandey1012@users.noreply.github.com>
2024-07-03 06:12:01 +0530

    Add files via upload
    create folder with name as files

---
+++ app.py
@@ -0,0 +1,143 @@
+from flask import Flask, request
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import CSVLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader,PyPDFLoader
+from langchain_community.document_loaders import UnstructuredFileLoader, UnstructuredMarkdownLoader,UnstructuredHTMLLoader
+import os
+
+app = Flask(__name__)
+
+folder_path = "db"
+
+cached_llm = Ollama(model="llama3")
+
+embedding = FastEmbedEmbeddings()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
+)
+
+DOCUMENT_MAPPING = {
+    ".html": UnstructuredHTMLLoader,
+    ".txt": TextLoader,
+    ".md": UnstructuredMarkdownLoader,
+    ".pdf": PyPDFLoader,
+    ".csv": CSVLoader,
+    ".xls": UnstructuredExcelLoader,
+    ".xlsx": UnstructuredExcelLoader,
+    ".docx": Docx2txtLoader,
+    ".doc": Docx2txtLoader,
+}
+
+raw_prompt = PromptTemplate.from_template(
+    """ 
+    <s>[INST] You are an intelligent chatbot that can answer user's queries.
+    You will be provided with Relevant context based on the user's queries.
+    Your task is to analyze user's query and generate response for the query utilizing the context.
+    Make sre to suggest one similar follow-up question based on the context for the user to ask.
+    
+    NEVER generate response to queries for which there is no or irrelevet context.
+    If you don't know the answer, just say that you don't know [/INST] </s>
+    [INST] {input}
+           Context: {context}
+           Answer:
+    [/INST]
+"""
+)
+
+
+@app.route("/ai", methods=["POST"])
+def aiPost():
+    print("Post /ai called")
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+    response = cached_llm.invoke(query)
+
+    print(response)
+
+    response_answer = {"answer": response}
+    return response_answer
+
+
+@app.route("/user_questions", methods=["POST"])
+def llmm_response():    
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+    print("Loading vector store")
+    vector_store = Chroma(persist_directory=folder_path, embedding_function=embedding)
+
+    print("Creating chain")
+    retriever = vector_store.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={
+            "k": 20,
+            "score_threshold": 0.1,
+        },
+    )
+
+    document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
+    chain = create_retrieval_chain(retriever, document_chain)
+
+    result = chain.invoke({"input": query})
+
+    print(result)
+
+    sources = []
+    for doc in result["context"]:
+        sources.append(
+            {"source": doc.metadata["source"], "page_content": doc.page_content}
+        )
+
+    response_answer = {"answer": result["answer"], "sources": sources}
+    return response_answer
+
+
+@app.route("/upload_docs", methods=["POST"])
+def upload_documents():
+    file = request.files["file"]
+    file_name = file.filename
+    save_file = "files/" + file_name
+    file.save(save_file)
+    file_extension = os.path.splitext(save_file)[1]
+    loader_class = DOCUMENT_MAPPING.get(file_extension)
+    if loader_class:
+        loader = loader_class(save_file)
+    
+    docs = loader.load_and_split()
+    print(f"docs len={len(docs)}")
+
+    chunks = text_splitter.split_documents(docs)
+    print(f"chunks len={len(chunks)}")
+
+    vector_store = Chroma.from_documents(
+        documents=chunks, embedding=embedding, persist_directory=folder_path
+    )
+
+    vector_store.persist()
+
+    response = {
+        "status": "Successfully Uploaded",
+        "filename": file_name,
+        "doc_len": len(docs),
+        "chunks": len(chunks),
+    }
+    return response
+
+
+def start_app():
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
+
+if __name__ == "__main__":
+    start_app()
