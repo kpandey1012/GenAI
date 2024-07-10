f51b06c77df3a63a8829c350bb6b2499f2990ebf
kpandey1012 <33618407+kpandey1012@users.noreply.github.com>
2024-07-11 04:48:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 from flask import Flask, request
 from langchain_community.llms import Ollama
+from langchain_community.chat_models import ChatOllama
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain.prompts import PromptTemplate
+from langchain.chains import create_retrieval_chain,create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.document_loaders import CSVLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader,PyPDFLoader
 from langchain_community.document_loaders import UnstructuredFileLoader, UnstructuredMarkdownLoader,UnstructuredHTMLLoader
 import os
+import time
+import textwrap
+
+
 
 app = Flask(__name__)
 
 folder_path = "db"
 
-cached_llm = Ollama(model="llama3")
 
-embedding = FastEmbedEmbeddings()
+from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
+
+chat_message_history = MongoDBChatMessageHistory(
+    session_id="test_session",
+    connection_string="mongodb://localhost:27017",
+    database_name="my_db",
+    collection_name="chat_histories",
+)
+
+local_llm = ChatOllama(model="phi3")
+
+embedding = OllamaEmbeddings(model='mxbai-embed-large')
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
+    chunk_size=1024, chunk_overlap=200, length_function=len, is_separator_regex=False
 )
 
 DOCUMENT_MAPPING = {
@@ -34,27 +50,27 @@ DOCUMENT_MAPPING = {
     ".doc": Docx2txtLoader,
 }
 
-raw_prompt = PromptTemplate.from_template(
-    """ 
-    <s>[INST] You are an intelligent chatbot that can answer user's queries.
-    You will be provided with Relevant context based on the user's queries.
-    Your task is to analyze user's query and generate response for the query utilizing the context.
-    Make sre to suggest one similar follow-up question based on the context for the user to ask.
-    
-    NEVER generate response to queries for which there is no or irrelevet context.
-    If you don't know the answer, just say that you don't know [/INST] </s>
-    [INST] {input}
-           Context: {context}
-           Answer:
-    [/INST]
-"""
-)
+
+@app.route("/general_quetions", methods=["POST"])
+def general_quetions():    
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+    response = local_llm.invoke(query)
+
+    print(response)
+
+    response_answer = {"answer": response}
+    return response_answer
 
 
 @app.route("/user_questions", methods=["POST"])
 def llmm_response():    
     json_content = request.json
     query = json_content.get("query")
+    userid = json_content.get("userid")
 
     print(f"query: {query}")
 
@@ -63,28 +79,68 @@ def llmm_response():
 
     print("Creating chain")
     retriever = vector_store.as_retriever(
-        search_type="similarity_score_threshold",
-        search_kwargs={
-            "k": 20,
-            "score_threshold": 0.1,
-        },
+        search_type="similarity",
+        search_kwargs={"k": 3}
+    )
+    print("Creating QA chain")
+    ### Contextualize question ###
+    contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+    )
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+    local_llm, retriever, contextualize_q_prompt
     )
 
-    document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
-    chain = create_retrieval_chain(retriever, document_chain)
+    ### Answer question ###
+    system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+    )
+    qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+    )
+    question_answer_chain = create_stuff_documents_chain(local_llm, qa_prompt)
 
-    result = chain.invoke({"input": query})
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-    print(result)
+    chain_with_history = RunnableWithMessageHistory(
+    rag_chain,
+    lambda session_id: MongoDBChatMessageHistory(
+        session_id=session_id,
+        connection_string="mongodb://localhost:27017",
+        database_name="my_db",
+        collection_name="chat_histories",
+    ),
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    )
+    config = {"configurable": {"session_id": userid}}
+    
+    print("getting response from llm")
 
-    sources = []
-    for doc in result["context"]:
-        sources.append(
-            {"source": doc.metadata["source"], "page_content": doc.page_content}
-        )
-
-    response_answer = {"answer": result["answer"], "sources": sources}
-    return response_answer
+    result = chain_with_history.invoke({"input": query}, config=config)
+    
+    return  result
 
 
 @app.route("/upload_docs", methods=["POST"])
@@ -109,7 +165,7 @@ def upload_documents():
     )
 
     vector_store.persist()
-
+    os.remove(save_file)
     response = {
         "status": "Successfully Uploaded",
         "filename": file_name,
